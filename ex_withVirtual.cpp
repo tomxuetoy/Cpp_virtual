6e7fda20b9bf77d73fbc26f61c3d7cc69905a5c6
Formal codes goto: tomxue <tomxue@outlook.com>
2013-09-07 14:55:16 +0800

    Create ex_withVirtual.cpp

---
+++ ex_withVirtual.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+ 
+class Parent
+{
+public:
+    Parent() {
+        cout<<"Parent Constructor"<<endl;
+    };
+    virtual ~Parent() {
+        cout<<"Parent Destructor"<<endl;
+    };
+};
+ 
+class Child : public Parent
+{
+public:
+    Child() {
+        cout<<"Child Constructor"<<endl;
+    };
+    ~Child() {
+        cout<<"Child Destructor"<<endl;
+    };
+};
+ 
+int main()
+{
+    Child* c_ptr = new Child;
+    delete c_ptr;
+    cout<<"-----------------"<<endl;
+    Parent* p_ptr = new Child;
+    delete p_ptr;
+    cout<<"-----------------"<<endl;
+    Child c;
+};
+/*
+$ a.exe
+Parent Constructor
+Child Constructor
+Child Destructor
+Parent Destructor
+-----------------
+Parent Constructor
+Child Constructor
+Child Destructor
+Parent Destructor
+-----------------
+Parent Constructor
+Child Constructor
+Child Destructor
+Parent Destructor
+ 
+ * */
